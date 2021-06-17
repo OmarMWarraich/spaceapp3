981907d92cdd58c2f928d561efb0681ad81ec68a
OmarMWarraich <earthling986@gmail.com>
2021-06-17 18:50:00 -0400

    Add bootstrap @types/bootstrap, Modify App.tsx, index.tsx, Launch.tsx, Launches.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const client = new ApolloClient({
+  uri: "https://spacexdata.herokuapp.com/graphql",
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
+      <ApolloHooksProvider client={client}>
     <App />
+    </ApolloHooksProvider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
